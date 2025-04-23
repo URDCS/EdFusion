ece92f11db67abba25d26e12ec7bff40efc6fd2f
AadarshAK <aadarshak.18@gmail.com>
2025-04-23 12:00:14 +0530

    Defined a funciton to get student marks as input Created dummy authentication.
    Co-authored-by: Ansh-yadav44 <Ansh-yadav44@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
 #Python file
+import csv
+print('Welcome to EdFusion!')
+userId = int(input('Enter your user Id'))
+Id = 348
+
+#This is where we will be defining all the functions
+def marks():
+    Physics = float(input("Enter marks in Physics"))
+    Chemistry = float(input("Enter marks in Chemistry"))
+    Mathametics = float(input("entter marks in Mathematics"))    
+    English = float(input("Enter marks in English"))
+    Computer_Science = float(input("enter marks in Computer Science"))
+    markslist = [Physics, Chemistry, Mathametics, English, Computer_Science]
+    return markslist
+
+if userId==348:
+    print('Logged in succesfully!')
+    roll = int(input('Enter student roll no : '))
+    #Function call for input of marks
+    print(marks())
+else:
+    print('Invalid credentials')
